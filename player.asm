b3e8acf658e88292cf4169441401fbfb25c0f0a1
hyphz <drmoose94@gmail.com>
2017-08-02 20:27:16 +0100

    Note timing wrong for lengths >1 ?

--- player.asm
+++ player.asm
@@ -416,17 +416,40 @@ outputBuffer            loop 67            ; 3 AYs (14 * 3) + 1 SID (25) = 67
                         lend
 
 
-Tempo                   db 13                              ; Number of frames per beat count
+Tempo                   db 7                              ; Number of frames per beat count
 
 music                   db 0, 0
-                        dw pattern
+                        dw pattern_arp
+                        db 0, 1
+                        dw pattern_bass
+                        db 16, 2
+                        dw pattern_mel
                       ;  db 2, 1
                       ;  dw pattern
                         db 255
 
 
-pattern                 db 0, 12, 1, 16, 1, 19, 1, 24
+; c  c#  d  d#  e  f  g  g# a  a# b
+; 0  1   2  3   4  5  6  7  8  9  10
+; 11 12  13 14  15 16 17 18 19 20 21
+; 22 23  24 25  26 27 28 29 30 31 32
+; 33 34  35 36  37 38 39 40 41 42 43
+; 44 45  46 47  48 49 50 51 52 53 54
+; 55 56  57 58  59 60 61 62 63 64 65
+; 66 67  68 69  70 71 72 73 74 75 76
+; 77 78  79 80  81 82 83 84 85 86 87
+
+
+pattern_bass            db 0, 0, 4, 5, 4, 6, 4, $80
+
+
+
+pattern_arp             db 0, 11, 1, 15, 1, 17, 1, 22
+                        db 1, 16, 1, 19, 1, 22, 1, 27
+                        db 1, 17, 1, 21, 1, 24, 1, 28
                         db 1, $80
+
+pattern_mel             db 0, 48, 3, 46, 1, 46, 3, 44, 1, 46, 4, $80
 
 
 
